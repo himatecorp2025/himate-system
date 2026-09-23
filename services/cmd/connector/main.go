a4c64bff7342ed1f9e859f4b25043cf8fb736d11
himatecorp2025 <himatecorp2025@gmail.com>
2026-09-23 19:58:29 +0200

    START-23.11.3i validate internal response release versions

--- services/cmd/connector/main.go
+++ services/cmd/connector/main.go
@@ -488,7 +488,7 @@ func (a *app) metrics(w http.ResponseWriter,r *http.Request){
 		req.Header.Set("Content-Type","application/json")
 		common.BindInternalRequest(req,a.internalToken)
 		req.Header.Set("X-Himate-User-ID","connector:"+c.CredentialID)
-		resp,err:=a.client.Do(req)
+		resp,err:=common.DoInternal(a.client, req)
 		if err!=nil { common.APIError(w,502,"IMPACT_UNAVAILABLE","Impact service did not respond");return }
 		resp.Body.Close()
 		if resp.StatusCode<200 || resp.StatusCode>=300 { common.APIError(w,409,"METRIC_REJECTED",fmt.Sprintf("Impact service rejected metric %d",i));return }
